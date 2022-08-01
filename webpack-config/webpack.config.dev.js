aa2fca492d63fed419c53e507fac4a19e9440545
김양하B <raya000@naver.com>
2022-08-01 12:56:49 +0900

    refactor: webpack version up 4 to 5 [참고링크] https://webpack.kr/migrate/5/#make-sure-your-build-has-no-errors-or-warnings https://webpack.kr/configuration/devtool/

--- webpack-config/webpack.config.dev.js
+++ webpack-config/webpack.config.dev.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-const webpackMerge = require('webpack-merge');
+const { merge } = require("webpack-merge");
 
-const commonConfig = require('./webpack.config.common');
-const helpers      = require('./helpers');
+const commonConfig = require("./webpack.config.common");
+const helpers = require("./helpers");
 
-module.exports = webpackMerge(commonConfig, {
-    mode: 'development',
+module.exports = merge(commonConfig, {
+  mode: "development",
 
-    devtool: 'cheap-module-eval-source-map',
+  devtool: "eval-cheap-source-map",
 
-    output: {
-        path: helpers.root('dist'),
-        publicPath: '/',
-        filename: '[name].bundle.js',
-        chunkFilename: '[id].chunk.js'
-    },
+  output: {
+    path: helpers.root("dist"),
+    publicPath: "/",
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].chunk.js",
+  },
 
-    optimization: {
-        noEmitOnErrors: true
-    },
+  optimization: {
+    noEmitOnErrors: true,
+  },
 
-    devServer: {
-        historyApiFallback: true,
-        stats: 'minimal'
-    }
+  devServer: {
+    historyApiFallback: true,
+    stats: "minimal",
+  },
 });
